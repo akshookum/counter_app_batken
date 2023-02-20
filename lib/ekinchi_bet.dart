fca3270f853fe3a923e7f752dce942ce5c474acb
User <user@Users-MacBook-Pro.local>
2023-02-20 16:27:13 +0600

    Counter app kurs batkeb done

---
+++ lib/ekinchi_bet.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class EkinchiBet extends StatelessWidget {
+  EkinchiBet({required this.sanKelet});
+  int sanKelet;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Center(
+          child: Text(
+            'Ekinchi bet',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12), color: Colors.grey),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 150, vertical: 15),
+                child: Text('San: $sanKelet'),
+              ),
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Icon(
+                  Icons.arrow_back_ios_new,
+                  size: 35,
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
